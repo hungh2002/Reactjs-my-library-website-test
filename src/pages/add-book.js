f45e81729dd8ba10f3ffd132c3e8341756b1961f
Lưu Mạnh Hùng <manhhung.h2002@gmail.com>
2023-11-01 06:38:50 +0900

    1

--- src/pages/add-book.js
+++ src/pages/add-book.js
@@ -1,23 +1,23 @@
-import toBase64 from "./../services/base64.js";
+import toBase64 from './../services/base64.js';
 
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
 
 const AddBook = () => {
-  const [thumbnail, setThumbnail] = useState("");
-  const handleOneChangeThumbnail = (file) => {
-    const data = toBase64(file);
-    return setThumbnail(data);
+  const [thumbnail, setThumbnail] = useState('');
+  const handleOneChangeThumbnail = async (file) => {
+    const data = await toBase64(file);
+    setThumbnail(`${data}`);
   };
 
   const handleSubmission = async (e) => {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
-    const title = formData.get("title");
-    const content = formData.get("content");
+    const title = formData.get('title');
+    const content = formData.get('content');
 
-    return await axios.post("http://localhost:8080/api/books", {
+    await axios.post('http://localhost:8080/api/books', {
       title: title,
       content: content,
       thumbnail: thumbnail,
@@ -26,25 +26,25 @@ const AddBook = () => {
 
   return (
     <form onSubmit={handleSubmission}>
-      <label htmlFor="title">Title:</label>
+      <label htmlFor='title'>Title:</label>
       <br />
-      <input type="text" name="title" />
+      <input type='text' name='title' />
       <br />
-      <label htmlFor="content">content:</label>
+      <label htmlFor='content'>content:</label>
       <br />
-      <input type="text" name="content" />
+      <input type='text' name='content' />
       <br />
-      <label htmlFor="thumbnail">Thumbnail:</label>
+      <label htmlFor='thumbnail'>Thumbnail:</label>
       <br />
       <input
-        type="file"
-        name="thumbnail "
+        type='file'
+        name='thumbnail '
         onChange={(e) => handleOneChangeThumbnail(e.target.files[0])}
       />
 
       <br />
       <br />
-      <input type="submit" value="Submit" />
+      <input type='submit' value='Submit' />
     </form>
   );
 };
